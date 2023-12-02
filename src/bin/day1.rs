a0979cc2087015f70033444786f9e349d05ed218
kiancm <kianmehrabani@gmail.com>
2023-12-02 02:17:16 -0500

    complete day 1 part 2

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,4 +1,8 @@
-use std::{fs, path::PathBuf};
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+    path::PathBuf,
+};
 
 fn main() -> std::io::Result<()> {
     let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
@@ -6,24 +10,72 @@ fn main() -> std::io::Result<()> {
 
     let input = fs::read_to_string(path)?;
 
-    let sum: u32 = input.lines()
-    .filter_map(|line| {
-        let nums: Vec<char> = line.chars().filter(|char| char.is_numeric()).collect();
-        match nums.as_slice() {
-            [only] => concat_digits(only.to_owned(), only.to_owned()),
-            [first, .., last] => concat_digits(first.to_owned(), last.to_owned()),
-            _ => None
-        }
-    })
-    .sum();
+    let digit_spellings = HashMap::from([
+        ("one", "1".to_string()),
+        ("two", "2".to_string()),
+        ("three", "3".to_string()),
+        ("four", "4".to_string()),
+        ("five", "5".to_string()),
+        ("six", "6".to_string()),
+        ("seven", "7".to_string()),
+        ("eight", "8".to_string()),
+        ("nine", "9".to_string()),
+    ]);
+
+    let substrings: HashSet<String> = digit_spellings
+        .keys()
+        .flat_map(|s| {
+            let mut substrings = Vec::new();
+            let mut substring = String::new();
+            for char in s.chars() {
+                substring.push(char);
+                substrings.push(substring.clone());
+            }
+            substrings
+        })
+        .collect();
+
+    let sum: u32 = input
+        .lines()
+        .filter_map(|line| {
+            let mut digits: Vec<String> = Vec::new();
+            let mut current = String::new();
+            for char in line.chars() {
+                if let Some(v) = digit_spellings.get(current.as_str()) {
+                    digits.push(v.to_owned());
+                    current = String::new();
+                }
+
+                if !substrings.contains(&current) {
+                    current = String::new();
+                }
+
+                if char.is_numeric() {
+                    digits.push(char.to_string());
+                } else {
+                    current.push(char);
+                }
+            }
+            if let Some(v) = digit_spellings.get(current.as_str()) {
+                digits.push(v.to_owned());
+            }
+            match digits.as_slice() {
+                [only] => concat_digits(only, only),
+                [first, .., last] => concat_digits(first, last),
+                _ => None,
+            }
+        })
+        .sum();
 
     println!("Sum: {:?}", sum);
 
     Ok(())
 }
 
-fn concat_digits(first: char, last: char) -> Option<u32> {
-    let mut str = first.to_string();
-    str.push(last);
+fn concat_digits(first: &String, last: &String) -> Option<u32> {
+    let mut str = String::new();
+    str.push_str(first);
+    str.push_str(last);
+
     str.parse().ok()
 }
