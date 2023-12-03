5ef9f824afe1a13b365209fc7b63b18be87ed186
kiancm <kianmehrabani@gmail.com>
2023-12-03 01:21:30 -0500

    implement day 2 part 2

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -5,40 +5,79 @@ const MAX_RED: u8 = 12;
 const MAX_GREEN: u8 = 13;
 const MAX_BLUE: u8 = 14;
 
+struct Game {
+    id: u8,
+    draws: Vec<CubeSet>,
+}
+
+struct CubeSet {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+impl CubeSet {
+    fn power(self) -> i32 {
+        self.red as i32 * self.green as i32 * self.blue as i32
+    }
+}
+
 fn main() -> std::io::Result<()> {
     let input = read_input(Day::DAY2)?;
 
-    let id_sum: i32 = input.lines().flat_map(parse_line)
-        .filter(|game| game.draws.iter().all(has_valid_cubes))
-        .map(|game| game.id)
-        .fold(0, |x, y| x as i32 + y as i32);
-
-    println!("ID Sum: {}", id_sum);
+    part1(&input);
+    part2(&input);
 
     Ok(())
 }
 
-fn has_valid_cubes(draw: &Draw) -> bool {
+fn part1(input: &String) {
+    let id_sum: i32 = input
+        .lines()
+        .flat_map(parse_line)
+        .filter(|game| game.draws.iter().all(has_valid_cubes))
+        .map(|game| game.id)
+        .fold(0, |x, y| x as i32 + y as i32);
+
+    println!("Part 1: {}", id_sum);
+}
+
+fn part2(input: &String) {
+    let power_sum: i32 = input
+        .lines()
+        .flat_map(parse_line)
+        .map(minimum_set)
+        .map(CubeSet::power)
+        .sum();
+
+    println!("Part 2: {}", power_sum);
+}
+
+fn minimum_set(game: Game) -> CubeSet {
+    let draws = game.draws;
+
+    let max_red = draws.iter().map(|c| c.red).max().unwrap_or_default();
+    let max_green = draws.iter().map(|c| c.green).max().unwrap_or_default();
+    let max_blue = draws.iter().map(|c| c.blue).max().unwrap_or_default();
+
+    CubeSet {
+        red: max_red,
+        green: max_green,
+        blue: max_blue,
+    }
+}
+
+fn has_valid_cubes(draw: &CubeSet) -> bool {
     draw.red <= MAX_RED && draw.green <= MAX_GREEN && draw.blue <= MAX_BLUE
-}
-
-struct Game {
-    id: u8,
-    draws: Vec<Draw>,
-}
-
-struct Draw {
-    red: u8,
-    green: u8,
-    blue: u8,
 }
 
 fn parse_line(line: &str) -> Option<Game> {
     let parts: Vec<&str> = line.split(":").collect();
     let id = parts.get(0).and_then(|s| {
         let sp = s.split(" ").collect::<Vec<_>>();
-        sp.into_iter().last()});
-    let draws: Vec<Draw> = parts
+        sp.into_iter().last()
+    });
+    let draws: Vec<CubeSet> = parts
         .get(1)
         .into_iter()
         .flat_map(|s| s.trim().split(";"))
@@ -49,7 +88,7 @@ fn parse_line(line: &str) -> Option<Game> {
         .map(|id| Game { id, draws })
 }
 
-fn parse_draw(raw: &str) -> Draw {
+fn parse_draw(raw: &str) -> CubeSet {
     let re = Regex::new(r#"([0-9]+) (red|blue|green)"#).ok();
 
     let pairs: Vec<(u8, &str)> = raw
@@ -78,9 +117,9 @@ fn parse_draw(raw: &str) -> Draw {
         .map(|(n, _)| n)
         .unwrap_or(&0);
 
-    Draw {
+    CubeSet {
         red: *red,
         green: *green,
-        blue: *blue
+        blue: *blue,
     }
 }
